12e4038f320f3af8c5bab82391f70dc7d006c339
Abiqb93 <ab.iqb93@gmail.com>
2024-10-09 15:54:45 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/dashboard/*" element={<Dashboard />} />
-      <Route path="/auth/*" element={<Auth />} />
-      <Route path="*" element={<Navigate to="/auth/sign-in" replace />} />
-    </Routes>
+    <BrowserRouter basename="/RacingWebsite"> {/* Add basename for GitHub Pages */}
+      <Routes>
+        <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/auth/*" element={<Auth />} />
+        <Route path="*" element={<Navigate to="/auth/sign-in" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
